eac6f4fa2e1e88e4847d86cc7d3a40cab3a2c411
Byron <byron@datameet.com>
2024-03-19 18:42:12 +1100

    adding react query

--- src/components/GameStuff/GameGrid.tsx
+++ src/components/GameStuff/GameGrid.tsx
@@ -1,35 +1,40 @@
-import { SimpleGrid, Text } from '@chakra-ui/react';
-import useGames from '../../hooks/useGames';
-import GameCard from './GameCard';
-import GameCardSkeleton from './GameCardSkeleton';
-import GameCardContainer from './GameCardContainer';
-import { GameQuery } from '../../App';
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
+import { GameQuery } from "../../App";
 
 interface Props {
-    gameQuery: GameQuery;
+  gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-    const { data, error, isLoading } = useGames(gameQuery);
-    const skeletons = [0]
-    for (let i = 1; i < 20; i++) skeletons.push(i);
+  const { data, error, isLoading } = useGames(gameQuery);
+  const skeletons = [0];
+  for (let i = 1; i < 24; i++) skeletons.push(i);
 
-    if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error}</Text>;
 
-    return (
-        <SimpleGrid
-            columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-            spacing={6}
-            padding='10px'
-        >
-            {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}>
-                <GameCardSkeleton></GameCardSkeleton>
-            </GameCardContainer>)}
-            {data.map(game => <GameCardContainer key={game.id}>
-                <GameCard game={game}></GameCard>
-            </GameCardContainer>)}
-        </SimpleGrid>
-    )
-}
+  return (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton></GameCardSkeleton>
+          </GameCardContainer>
+        ))}
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game}></GameCard>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
+  );
+};
 
-export default GameGrid
+export default GameGrid;
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,23 @@ import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+interface Query {
+  retry: number;
+  staleTime: number;
+  cacheTime: number;
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      staleTime: 10 * 1000,
+      cacheTime: 300_000,
+    } as Query,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,6 +29,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
+        <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>
