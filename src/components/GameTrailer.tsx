d5632503a258b1e610f81e6f4e50f2266b7ca8a5
Byron <byron@datameet.com>
2024-04-16 13:10:22 +1000

    fixed navbar, refactored entities

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,24 @@
+import { Text } from "@chakra-ui/react";
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+
+  return first ? (
+    <video src={first?.data[480]} poster={first.preview} controls />
+  ) : (
+    <Text>No Trailer</Text>
+  );
+};
+
+export default GameTrailer;
